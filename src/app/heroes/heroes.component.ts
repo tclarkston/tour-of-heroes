b848f56a0365204bfab60c98fe8b9b279b92fe8f
Todd Clarkston <todd@seriescode.com>
2019-12-28 14:20:19 -0700

    Initital commit of application

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../models/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
