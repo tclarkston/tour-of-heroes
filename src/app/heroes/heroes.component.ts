a2456f835ef791c28edd501cfadd38d86a24db8b
Todd Clarkston <todd@seriescode.com>
2019-12-28 16:21:11 -0700

    Lesson 3 updates

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../models/mock-heroes';
+import { Hero } from '../models/hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
+
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  onSelect(hero: Hero){
+    if (this.selectedHero === hero){
+      this.selectedHero = null;  
+    }
+    else{
+      this.selectedHero = hero;
+    }
+  }
 }
